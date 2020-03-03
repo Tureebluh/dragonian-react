c38c242e1a5858c673433f2eda0e44d2aaa0bb26
Jarek Thomas <jarek.thomas@outlook.com>
2020-03-03 10:57:16 -0800

    removed all src folder from root dir

--- src/collab/CollabOnLoad.js
+++
@@ -1,8 +0,0 @@
-import UnassignedRole from './UnassignedRole';
-
-var onload = () => {
-    var unassignedRoles = '<h1>Page Currently Under Construction!</h1>';
-    document.querySelector('#collabsDiv').innerHTML = unassignedRoles;
-}
-
-export default onload;
